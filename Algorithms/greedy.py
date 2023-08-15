90792d5661257652dbd800f6ee1c3eb21cf25d09
sranucci <86986069+sranucci@users.noreply.github.com>
2023-08-14 21:32:37 -0300

    implemented greedy iterative approach

--- Algorithms/greedy.py
+++ Algorithms/greedy.py
@@ -1,21 +1,22 @@
 import time
 
-
 from src.sokoban import *
 from src.BoardTree import BoardTreeNode
 
-def greedy():
 
-    game = Sokoban("../Boards/board4.txt")
+def greedy():
+    game = Sokoban("../Boards/board.txt")
     print("running")
 
     mainNode = BoardTreeNode(game)
 
     start = time.time()
-    result = mainNode.expand()
+    result = mainNode.expand() #version reecursiva
+    #result = mainNode.iterative_expand() #version iterativa
     end = time.time()
+    #print(f"{result.moves}\ntook {end - start}s")
     return end, start, result
+
 
 # print(f"time it took: {end - start}")
 # print(result.moves if result else "No solution was found")
-
--- src/BoardTree.py
+++ src/BoardTree.py
@@ -1,3 +1,5 @@
+import queue
+
 from src.sokoban import *
 
 
@@ -8,7 +10,7 @@ class BoardTreeNode:
         self.board = board
         self.children: list[tuple[int, BoardTreeNode, Moves]] = []
 
-    def expand(self) -> Sokoban:
+    def expand(self) -> Sokoban | None:
         # creo un nodo hijo por cada movimiento
         if self.board.victory():
             return self.board
@@ -31,4 +33,32 @@ class BoardTreeNode:
             if result:
                 return result
 
-        #return None implicito
+        return None
+
+    def iterative_expand(self) -> Sokoban | None:
+        visited_states = set()
+        stack = [self]
+
+        while stack:
+            node = stack.pop()
+
+            if node.board.victory():
+                return node.board
+
+            if hash(node.board) not in visited_states:
+                visited_states.add(hash(node.board))
+
+                for move in node.board.get_valid_moves():
+                    alt_game = copy.deepcopy(node.board)
+                    alt_game.move(move)
+
+                    if hash(alt_game) not in visited_states:
+                        game_node = BoardTreeNode(alt_game)
+                        current_distance = alt_game.calculate_manhattan_distance()
+                        node.children.append((current_distance, game_node, move))
+
+                node.children.sort(key=lambda item: item[0])
+                for _, childNode, _ in reversed(node.children):
+                    stack.append(childNode)
+
+        return None
