2a4ebec4c1eec560a172b23954091fa881e3a953
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-08-15 16:24:43 -0300

    feat: algorithm time comparison, a* is now function

--- Data-Colection/algorithm-times.py
+++ Data-Colection/algorithm-times.py
@@ -1,52 +1,107 @@
-import subprocess
+import math
+
 from Algorithms.bfs import bfs
 from Algorithms.dfs import dfs
 from Algorithms.greedy import greedy
+from Algorithms.a_star import a_star
 import csv
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.colors as pc
+import numpy as np
 
 
 board = "../Boards/board2.txt"
 ITERATIONS = 100
-bfs_time = 0
-dfs_time = 0
-greedy_time = 0
-for _ in range(ITERATIONS):
-    #BFS
-    bfs_end_time, bfs_start_time, _, _, _, _ = bfs(board)
-
-    bfs_time += bfs_end_time - bfs_start_time
-
-    #DFS
-    dfs_end_time, dfs_start_time, _, _, _, _ = dfs(board)
-
-    dfs_time += dfs_end_time - dfs_start_time
-
-    #GREEDY
-    greedy_end_time, greedy_start_time, result = greedy(board)
-
-    greedy_time += greedy_end_time - greedy_start_time
-
-bfs_time /= ITERATIONS
-dfs_time /= ITERATIONS
-greedy_time /= ITERATIONS
 
 with open(f"../Results/algorithm-times.csv", "w") as f:
-    print(f"BFS, {round(bfs_time,5)}", file=f)
-    print(f"DFS, {round(dfs_time,5)}", file=f)
-    print(f"GREEDY, {round(greedy_time,5)}", file=f)
+    for _ in range(ITERATIONS):
+        #BFS
+        bfs_end_time, bfs_start_time, solution_exists, _, _, _ = bfs(board)
+        print(solution_exists)
 
-data = []
+        bfs_time = bfs_end_time - bfs_start_time
+        print(f"BFS, {bfs_time}", file=f)
+
+    for _ in range(ITERATIONS):
+        #DFS
+        dfs_end_time, dfs_start_time, solution_exists, _, _, _ = dfs(board)
+
+        dfs_time = dfs_end_time - dfs_start_time
+        print(f"DFS, {dfs_time}", file=f)
+
+    for _ in range(ITERATIONS):
+        #GREEDY
+        greedy_end_time, greedy_start_time, result = greedy(board)
+
+        greedy_time = greedy_end_time - greedy_start_time
+        print(f"GREEDY, {greedy_time}", file=f)
+    for _ in range(ITERATIONS):
+        #A*
+        Astar_end_time, Astar_start_time, solution_exists, _, _, _ = a_star(board)
+
+        Astar_time = Astar_end_time - Astar_start_time
+        print(f"A_STAR, {Astar_time}", file=f)
+
+
+bfs_data = []
+dfs_data = []
+greedy_data = []
+Astar_data = []
 with open(f"../Results/algorithm-times.csv", "r") as timedata:
     reader = csv.reader(timedata)
-
     for algorithm, time in reader:
-        data.append({'Algorithm': algorithm, 'Time': float(time)})
+        if algorithm == 'BFS':
+            bfs_data.append(float(time))
+        elif algorithm == 'DFS':
+            dfs_data.append(float(time))
+        elif algorithm =='A_STAR':
+            Astar_data.append(float(time))
+        else:
+            greedy_data.append(float(time))
 
-df = pd.DataFrame(data)
+bfs_data = np.array(bfs_data)
+dfs_data = np.array(dfs_data)
+greedy_data = np.array(greedy_data)
+Astar_data = np.array(Astar_data)
 
-df['Time'] = df['Time'].apply(lambda x: round(x, 5))  # Round 'Time' values
+bfs_mean = np.mean(bfs_data)
+dfs_mean = np.mean(dfs_data)
+greedy_mean = np.mean(greedy_data)
+Astar_mean = np.mean(Astar_data)
 
-fig = px.bar(df, x="Algorithm", y="Time", color="Algorithm", title="Time taken for algorithms in trivial map")
+bfs_error = np.std(bfs_data) / math.sqrt(ITERATIONS)
+dfs_error = np.std(dfs_data) / math.sqrt(ITERATIONS)
+greedy_error = np.std(greedy_data) / math.sqrt(ITERATIONS)
+Astar_error = np.std(Astar_data) / math.sqrt(ITERATIONS)
+
+algorithms = ['BFS', 'DFS', 'GREEDY', 'A*']
+x_pos = np.arange(len(algorithms))
+CTEs = [bfs_mean, dfs_mean, greedy_mean, Astar_mean]
+error = [bfs_error, dfs_error, greedy_error, Astar_error]
+
+num_colors = len(algorithms)
+distinct_colors = pc.qualitative.Plotly * int(np.ceil(num_colors / len(pc.qualitative.Plotly)))
+
+# Create a bar chart with error bars
+data = [
+    go.Bar(
+        x=algorithms,
+        y=CTEs,
+        error_y=dict(type='data', array=error, visible=True),
+        marker_color=distinct_colors,
+        opacity=0.6
+    )
+]
+
+layout = go.Layout(
+    title='Algorithm Performance Comparison',
+    xaxis=dict(title='Algorithms'),
+    yaxis=dict(title='Time'),
+    showlegend=False
+)
+
+fig = go.Figure(data=data, layout=layout)
 fig.show()
+
