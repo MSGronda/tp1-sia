27bb2cc1fd02cb754beef603ca56ee060f0e9e52
Marcos Gronda <marcosgronda@gmail.com>
2023-08-19 11:02:12 -0300

    refactor: heuristic comparison

--- Data-Colection/heuristics-comparison.py
+++ Data-Colection/heuristics-comparison.py
@@ -4,61 +4,39 @@ import plotly.express as px
 import check
 from Algorithms import greedy, a_star
 
+from Algorithms.Heuristics.combined_heuristic import combined_heuristic
+from Algorithms.Heuristics.lost_game import lost_game_heuristic
 from Algorithms.Heuristics.manhattan_distance import calculate_manhattan_distance
 from Algorithms.Heuristics.euclidian_distance import calculate_euclidian_distance
 from Algorithms.Heuristics.average_distance import calculate_average_distance
-
+from Algorithms.Heuristics.manhattan_wall_detection import calculate_manhattan_distance_wall_detection
 
 check.check_results_directory()
 
-board = "../Boards/board_benchmark.txt"
-ITERATIONS = 10
+board = "../Boards/board.txt"
+ITERATIONS = 1
 
-# heuristics = ['manhattan', 'euclidian', 'average']
-# algorithms = ['']
+heuristic_array = [
+    (calculate_manhattan_distance, 'manhattan'), (calculate_euclidian_distance, 'euclidian'), (calculate_average_distance, 'max (euclidian and manhattan)'),
+    (calculate_manhattan_distance_wall_detection, 'manhattan-wall'), (lost_game_heuristic, 'lost-game'), (combined_heuristic, 'combined (manhattan and lost-game)')
+]
 
 with open(f"../Results/heuristic-comparison.csv", "w") as f:
-    print("With Manhattan")
-    print("Running Greedy")
-    for _ in range(ITERATIONS):
-        greedy_end_time, greedy_start_time, result = greedy.greedy(board, calculate_manhattan_distance)
-        greedy_time = greedy_end_time - greedy_start_time
-        print(f"Manhattan, GREEDY, {greedy_time}", file=f)
+    for heuristic, name in heuristic_array:
+        print(f"With {name}")
+        print("  Running Greedy")
+        for _ in range(ITERATIONS):
+            greedy_end_time, greedy_start_time, result = greedy.greedy(board, heuristic)
+            greedy_time = greedy_end_time - greedy_start_time
+            print(f"{name}, GREEDY, {greedy_time}", file=f)
 
-    print("Running A*")
-    for _ in range(ITERATIONS):
-        Astar_end_time, Astar_start_time, solution_exists, _, _, _ = a_star.a_star(board, calculate_manhattan_distance)
+        print("  Running A*")
+        for _ in range(ITERATIONS):
+            Astar_end_time, Astar_start_time, solution_exists, _, _, _ = a_star.a_star(board, heuristic)
+            Astar_time = Astar_end_time - Astar_start_time
+            print(f"{name}, A_STAR, {Astar_time}", file=f)
 
-        Astar_time = Astar_end_time - Astar_start_time
-        print(f"Manhattan, A_STAR, {Astar_time}", file=f)
 
-    print("With Euclidian")
-    print("Running Greedy")
-    for _ in range(ITERATIONS):
-        greedy_end_time, greedy_start_time, result = greedy.greedy(board, calculate_euclidian_distance)
-        greedy_time = greedy_end_time - greedy_start_time
-        print(f"Euclidian, GREEDY, {greedy_time}", file=f)
-
-    print("Running A*")
-    for _ in range(ITERATIONS):
-        Astar_end_time, Astar_start_time, solution_exists, _, _, _ = a_star.a_star(board, calculate_euclidian_distance)
-
-        Astar_time = Astar_end_time - Astar_start_time
-        print(f"Euclidian, A_STAR, {Astar_time}", file=f)
-
-    print("With Average")
-    print("Running Greedy")
-    for _ in range(ITERATIONS):
-        greedy_end_time, greedy_start_time, result = greedy.greedy(board, calculate_average_distance)
-        greedy_time = greedy_end_time - greedy_start_time
-        print(f"Average, GREEDY, {greedy_time}", file=f)
-
-    print("Running A*")
-    for _ in range(ITERATIONS):
-        Astar_end_time, Astar_start_time, solution_exists, _, _, _ = a_star.a_star(board, calculate_average_distance)
-
-        Astar_time = Astar_end_time - Astar_start_time
-        print(f"Average, A_STAR, {Astar_time}", file=f)
 
 data = []
 with open(f"../Results/heuristic-comparison.csv", "r") as heuristics_data:
